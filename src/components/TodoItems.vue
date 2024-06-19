e69344ed7ac7bc5c66d64e3089d2fe897ea3f214
thyagomac <thyago.cormick@gmail.com>
2024-06-19 16:40:37 -0300

    components for todo;

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="space-y-2">
+    <TodoItem />
+  </div>
+</template>
+<script>
+import TodoItem from "@/components/TodoItem.vue";
+
+export default {
+  components: {
+    TodoItem,
+  },
+};
+</script>
+<style></style>
